d9bff2734bce6507d4b1c395ed21d9512fe21de3
caio1459 <caiogrechic50@gmail.com>
2023-10-23 13:48:02 -0300

    Finalizando a primeira aula do curso

---
+++ src/app.js
@@ -0,0 +1,16 @@
+//Importa o express
+const express = require("express");
+//Cria uma intancia do express
+const app = express();
+//Define uma porta
+const port = 3000;
+
+//Criar rota padrão
+app.get("/", (request, response) => {
+  response.send("Rota acessada com sucesso");
+});
+
+//Escutar a porta criada
+app.listen(port, () => {
+  console.log(`Servidor rodando no endereço: http://localhost:${port}`);
+});
