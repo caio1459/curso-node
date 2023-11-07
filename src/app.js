285e9b164fd807e2a41738730ab3cd87d493a73a
caio1459 <caiogrechic50@gmail.com>
2023-11-07 13:36:24 -0300

    Terminado as rotas de post e delete

--- src/app.js
+++ src/app.js
@@ -13,18 +13,55 @@ const selecoes = [
   { id: 4, nome: "Portugal", grupo: "B" },
 ];
 
+//Funções auxiliares
+function buscarSelecaoPorId(id) {
+  return selecoes.filter(selecao => selecao.id == id)
+}
+
+function buscarIndex(id) {
+  return selecoes.findIndex(selecao => selecao.id == id)
+}
+
 //Criar rota padrão
 app.get("/", (req, res) => {
   res.send("Rota acessada com sucesso");
 });
 
+//Listar todos
 app.get("/selecoes", (req, res) => {
   res.status(200).send(selecoes);
 });
 
+//Criar seleção
 app.post("/selecoes", (req, res) => {
   selecoes.push(req.body);
-  res.status(201).send("Seleção cadastrada com sucesso!");
+  // res.status(201).send("Seleção cadastrada com sucesso!");
+  res.json(selecoes)
 });
+
+//Listar seleção
+app.get("/selecoes/:id", (req, res) => {
+  let id = req.params.id
+  res.json(buscarSelecaoPorId(id)).status(200)
+})
+
+//Excluir seleção
+app.delete("/selecoes/:id", (req, res) => {
+  let index = buscarIndex(req.params.id)
+  selecoes.splice(index, 1) //Corta um elemento de um array
+  if (selecoes[index]) {
+    res.status(202).send(`Seleção com id ${req.params.id} excluida com sucesso`)
+  } else {
+    res.send("Seleção não encontrada")
+  }
+})
+
+app.put("/selecoes/:id", (req, res) => {
+  let index = buscarIndex(req.params.id)
+  //Conteudo da seleção de acordo com o index é substituido pelo parametro que está sendo criado no corpo da requisição
+  selecoes[index].nome = req.body.nome
+  selecoes[index].grupo = req.body.grupo
+  res.json(selecoes)
+})
 
 export default app;
